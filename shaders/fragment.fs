a0fa4ba2ac05b56c10608df4d311efa4bd0305af
Glen Maddern <glenmaddern@gmail.com>
2013-10-04 13:44:34 +1000

    really getting somewhere!

--- shaders/fragment.fs
+++ shaders/fragment.fs
@@ -1,13 +1,13 @@
 precision mediump float;
 
 void main() {
-    float r = 1.0;
-    float g = 1.0;
-    float b = 1.0;
-    float a = 0.8;
+  float r = 1.0;
+  float g = 1.0;
+  float b = 1.0;
+  float a = 0.8;
 
-    css_ColorMatrix = mat4( r  , 0.0, 0.0, 0.0,
-                            0.0, g  , 0.0, 0.0,
-                            0.0, 0.0, b  , 0.0,
-                            0.0, 0.0, 0.0, a );
+  css_ColorMatrix = mat4( r  , 0.0, 0.0, 0.0,
+                          0.0, g  , 0.0, 0.0,
+                          0.0, 0.0, b  , 0.0,
+                          0.0, 0.0, 0.0, a );
 }
--- shaders/vertex.vs
+++ shaders/vertex.vs
@@ -1,10 +1,13 @@
 precision mediump float;
 
 attribute vec4 a_position;
+attribute vec2 a_texCoord;
 uniform mat4 u_projectionMatrix;
+uniform mat4 matrix;
 
 void main() {
+  vec4 position = a_position;
+  float angle = 0.5;
 
-    gl_Position = u_projectionMatrix * a_position;
-
+  gl_Position = u_projectionMatrix * matrix * a_position;
 }
