ba1b9886e88c00ad2419c7a5faaf0f8ed7b0fa77
Glen Maddern <glenmaddern@gmail.com>
2013-10-04 12:11:15 +1000

    shader party

---
+++ shaders/fragment.fs
@@ -0,0 +1,13 @@
+precision mediump float;
+
+void main() {
+    float r = 1.0;
+    float g = 1.0;
+    float b = 1.0;
+    float a = 0.8;
+
+    css_ColorMatrix = mat4( r  , 0.0, 0.0, 0.0,
+                            0.0, g  , 0.0, 0.0,
+                            0.0, 0.0, b  , 0.0,
+                            0.0, 0.0, 0.0, a );
+}
